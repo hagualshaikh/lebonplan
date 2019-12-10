5a54816ed966050cdfe70146f9e314ff8c4809de
pc <pc@aba-firas.local>
2019-12-10 15:23:01 +0100

    structure projet

---
+++ index.js
@@ -0,0 +1,24 @@
+var express = require("express");
+var exphbs = require("express-handlebars");
+var mongoose = require("mongoose");
+
+var bodyParser = require("body-parser");
+
+var port = process.env.PORT || 3000;
+
+var app = express();
+
+app.use(express.static('public'));
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.get('/', function(req, res){
+    console.log('GET /')
+    res.render('home');
+});
+
+app.listen(port, function () {
+    console.log("Server started on port:", port);
+});
