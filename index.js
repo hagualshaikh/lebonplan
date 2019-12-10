9a2846f0f01ecfb939d71004538986e084d51929
dechenkama <dechenwangmogmail.com@dechens-MacBook-Pro.local>
2019-12-10 16:34:36 +0100

    Page products

--- index.js
+++ index.js
@@ -12,13 +12,21 @@ app.use(express.static('public'));
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/', function(req, res){
+app.get('/', function(req, res) {
     console.log('GET /')
     res.render('home');
 });
 
-app.listen(port, function () {
+app.get('/products', function(req, res) {
+    console.log('GET /products')
+    res.render('products');
+});
+
+
+
+
+app.listen(port, function() {
     console.log("Server started on port:", port);
 });
